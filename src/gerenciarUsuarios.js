20b6b719b44494f649724693862272464f71bd97
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-09-09 22:56:15 -0300

    Mudei a lista de nome de usuários para se tornarem objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,11 +1,34 @@
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+//lista de usuário que contém nome e e-mail
+const usuarios = // Olha JavaScripton isso é uma lista de objetos!!!!!!!!!!
+    // E o kiko? O J
+    [
+        {
+            nome: 'Matheus',
+            email: 'matheus@email.com'
+        },
+
+        {
+            nome: 'Marcos',
+            email: 'marcos@email.com'
+        },
+
+        {
+            nome: 'Lucas',
+            email: 'lucas@email.com'
+        },
+
+        {
+            nome: 'João',
+            email: 'joao@email.com'
+        }
+];
 
 function retornarUsuarios() {
     return usuarios;
 }
 
-function adicionarNovoUsuario(nome) { //é nome, porque você precisa de um nome para adicionar na lista, representação do nomeup -- não importa qual o nome da entrada, mas é a definição que você usa pra dizer o que você precisa.
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario) { //dentro da lista de usuário, coloque um usuário no final.
+    usuarios.push(usuario);
 }
 
 module.exports = {
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -3,13 +3,26 @@ const { expect } = require('chai');
 
 describe ('Testar as funções de Gestão de Usuários', function (){
     it('Validar que posso adicionar um novo nome de usuário na lista', function(){
-        //1. Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario('Maria');
+        //1. Adicionar um novo usuário na lista de usuários
+        adicionarNovoUsuario(
+            {
+            nome: 'Maria',
+            email: 'maria@email.com'
+            }
+        );
 
         //2. Retornar a lista de usuários na caixa lista de usuários
         const listaUsuarios = retornarUsuarios();
 
         //3. Comparar se o novo nome estpa no fim da lista de usuários
-        expect(listaUsuarios.at(-1)).to.equal('Maria');
+        // expect(listaUsuarios.at(-1).nome).to.equal('Maria');
+        // expect(listaUsuarios.at(-1).email).to.equal('maria@email.com');
+
+        expect(listaUsuarios.at(-1)).to.eql(
+            {
+            nome: 'Maria',
+            email: 'maria@email.com'
+            }
+        )
     });
 });
